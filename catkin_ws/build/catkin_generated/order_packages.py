67e961f3ee90a388f7877dd33839042007b53829
FraSave1612 <fsaverio9050@yahoo.it>
2024-03-09 06:36:06 -0800

    primo push

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/fra/gitHub/potential-robot/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/fra/gitHub/potential-robot/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/fra/gitHub/potential-robot/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_ws/build/robot_project_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fra/gitHub/potential-robot/catkin_ws/src/robot_project_pkg
+# Build directory: /home/fra/gitHub/potential-robot/catkin_ws/build/robot_project_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
